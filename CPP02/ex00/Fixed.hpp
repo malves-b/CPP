7e4d7fbd08ae9e155cd3787f6425f9a2a8a927b2
Matheus Alves Barbosa Castro <malves-b@student.42porto.com>
2025-03-28 20:42:44 +0000

    Add ex00

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Fixed
+{
+private:
+	/* data */
+public:
+	Fixed(/* args */);
+	~Fixed();
+};
+
+Fixed::Fixed(/* args */)
+{
+}
+
+Fixed::~Fixed()
+{
+}
